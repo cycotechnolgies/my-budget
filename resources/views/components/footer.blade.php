7fa537cd77da67729512ca581df03f1f728c622c
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2025-07-31 22:01:58 +0530

    laravel breeze setup with KUI and miner changes

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>Developed </span>
+
+        <span>by</span>
+
+        <a
+            href=""
+            target="_blank"
+            class="text-blue-600 hover:underline"
+        >
+            CYCO TECHNOLOGIES
+        </a>
+    </p>
+</footer>
